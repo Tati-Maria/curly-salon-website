bfa0dccb97e438891a8571ecd0c680f346c21685
Maria Tati <mauramaria92@gmail.com>
2023-01-11 20:34:44 +0000

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,8 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        "hero-contact": "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(../images/salon.jpg)",
-        "hero-header" : "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(../images/hero-1.jpg)"
+        "heroContact": "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(../images/salon.jpg)",
+        "hero" : "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(../images/hero-1.jpg)"
     },
   },
   plugins: [],
